6fd3c97d1a6da3d0cee24b52a32145961407240d
정진규 <monibu1548@gmail.com>
2024-06-09 16:02:29 +0900

    라우터에 인증 미들웨어 적용

--- supabase/functions/api/routers/userRouter.ts
+++ supabase/functions/api/routers/userRouter.ts
@@ -1,8 +1,11 @@
+import { authMiddleware } from "../middleware/authMiddleware.ts";
 import { UserController } from "./../controllers/userController.ts";
 import { Hono } from "https://deno.land/x/hono/mod.ts";
 
 const userRouter = new Hono();
 const userController = new UserController();
+
+userRouter.use(authMiddleware);
 
 userRouter.post(
   "/v1/fcmToken",
